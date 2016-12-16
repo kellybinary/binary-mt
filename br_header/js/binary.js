099a6e0493b25ef147bb39d2668e59ead544b1dd
kellybinary <kelly@binary.com>
2016-12-16 16:01:40 +0800

    Auto-generated commit

--- br_header/js/binary.js
+++ br_header/js/binary.js
@@ -17351,50 +17351,6 @@ for (var key in texts_json) {
     }
 }
 
-;var $languages,
-    languageCode,
-    languageText;
-
-function create_language_drop_down(languages) {
-    $languages = $('.languages');
-    var selectLanguage = 'ul#select_language',
-        $selectLanguage = $languages.find(selectLanguage);
-    if ($languages.length === 0 || $selectLanguage.find('li span.language').text() !== '') return;
-    languages.sort(function(a, b) {
-        return (a === 'EN' || a < b) ? -1 : 1;
-    });
-    var displayLanguage = 'ul#display_language',
-        language = page.language();
-    languageCode = language && language !== '' ? language : 'en';
-    languageText = language && language !== '' ? map_code_to_language(language) : 'English';
-    add_display_language(displayLanguage);
-    add_display_language(selectLanguage);
-    for (var i = 0; i < languages.length; i++) {
-        if (languages[i] !== 'JA') {
-            $selectLanguage.append('<li class="' + languages[i] + '">' + map_code_to_language(languages[i]) + '</li>');
-        }
-    }
-    $selectLanguage.find('li.' + language + ':eq(1)').addClass('invisible');
-    page.on_change_language();
-    $('.languages').removeClass('invisible');
-}
-
-function add_display_language(id) {
-    $languages.find(id + ' li')
-              .addClass(languageCode)
-              .find('span.language')
-              .text(languageText);
-}
-
-function map_code_to_language(code) {
-    var map = page.all_languages();
-    return map[code];
-}
-
-module.exports = {
-    create_language_drop_down: create_language_drop_down,
-};
-
 ;var CommonData = {
     getLoginToken: function() { return Cookies.get('login'); }
 };
@@ -17647,8 +17603,7 @@ pjax_config_page("/terms-and-conditions", function() {
     };
 });
 
-;var create_language_drop_down = require('../common_functions/attach_dom/language_dropdown').create_language_drop_down;
-/*
+;/*
  * It provides a abstraction layer over native javascript Websocket.
  *
  * Provide additional functionality like if connection is close, open
@@ -17818,7 +17773,6 @@ function BinarySocketClass() {
                     page.client.check_tnc();
                 } else if (type === 'website_status') {
                     if(!response.hasOwnProperty('error')) {
-                        create_language_drop_down(response.website_status.supported_languages);
                         LocalStore.set('website.tnc_version', response.website_status.terms_conditions_version);
                         page.client.check_tnc();
                         if (response.website_status.hasOwnProperty('clients_country')) {
