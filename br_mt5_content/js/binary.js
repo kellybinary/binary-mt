a7da809730c5da7a07fa31901f9944521127e61e
kellybinary <kelly@binary.com>
2016-12-19 17:48:37 +0800

    Auto-generated commit

--- br_mt5_content/js/binary.js
+++ br_mt5_content/js/binary.js
@@ -18420,11 +18420,31 @@ var BinarySocket = new BinarySocketClass();
         if(/demo/.test(accType)) {
             if(!hasMTDemo) {
                 console.log(accType);
-                displayAccount(volatility);
+                //displayAccount(volatility);
                 // indInSection(accType, '.account-details');
                 // $form = findInSection(accType, '.form-new-account');
                 // $form.removeClass(hiddenClass);
                 // $form.find('.name-row').removeClass(hiddenClass);
+                     findInSection(accType, '.form-new-account').addClass(hiddenClass);
+        var mtWebURL = 'https://trade.mql5.com/trade?servers=Binary.com-Server&amp;trade_server=Binary.com-Server&amp;';
+        var $details = $('<div/>').append($(
+            // makeTextRow('Login ID', mt5Accounts[accType].login) +
+            // makeTextRow('Balance', currency + ' ' + mt5Accounts[accType].balance, 'balance') +
+            makeTextRow('Name', mt5Accounts[accType].name) +
+            // makeTextRow('Leverage', mt5Accounts[accType].leverage)
+            makeTextRow('', text.localize('Start trading with MetaTrader 5:') + '<div class="download gr-padding-10">' +
+                '<a class="button pjaxload" href="' + page.url.url_for('download-metatrader') + '">' +
+                    '<span>' + text.localize('Download desktop app') + '</span></a>' +
+                '<a class="button" href="' + (mtWebURL + 'login=' + mt5Accounts[accType].login) + '" target="_blank">' +
+                    '<span>' + text.localize('Go to web terminal') + '</span></a><br />' +
+                '<a href="https://download.mql5.com/cdn/mobile/mt5/ios?server=Binary.com-Server" target="_blank">' +
+                    '<div class="app-store-badge"></div>' +
+                '</a>' +
+                '<a href="https://download.mql5.com/cdn/mobile/mt5/android?server=Binary.com-Server" target="_blank">' +
+                    '<div class="google-play-badge"></div>' +
+                '</a></div>')
+        ));
+        findInSection(accType, '.account-details').html($details.html());
             }
         } else if(/financial|volatility/.test(accType)) {
             if(!mt5Accounts.hasOwnProperty(accType)) {
