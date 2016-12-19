a2894d770bd9afc9a8845cde5bd6ec6a2e98bc02
kellybinary <kelly@binary.com>
2016-12-19 14:02:38 +0800

    Auto-generated commit

--- br_mt5_content/js/binary.js
+++ br_mt5_content/js/binary.js
@@ -18433,7 +18433,7 @@ var BinarySocket = new BinarySocketClass();
                     });
 
                     findInSection(accType, '.msg-account').html(hasRealBinaryAccount ?
-                        text.localize('To create a real account for MetaTrader, switch to your [_1] real money account.', ['Binary.com']) :
+                        text.localize('To create a Financial Account for MT5, please switch to your [_1] Real Account.', ['Binary.com']) :
                         text.localize('To create a real account for MetaTrader, <a href="[_1]">upgrade to [_2] real money account</a>.', [page.url.url_for('new_account/realws', '', true), 'Binary.com'])
                     ).removeClass(hiddenClass);
                 } else {
