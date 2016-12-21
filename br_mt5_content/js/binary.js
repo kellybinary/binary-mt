1c32278bc719efefd6e0681610187541c1a51f00
kellybinary <kelly@binary.com>
2016-12-21 10:28:30 +0800

    Auto-generated commit

--- br_mt5_content/js/binary.js
+++ br_mt5_content/js/binary.js
@@ -16152,7 +16152,9 @@ Client.prototype = {
             $('#metatrader p').addClass('center-text')
                 .html(text.localize('Your existing Binary.com account and cashier will be linked to your MT5 account.'));
             $('#content > .container').addClass('center-text')
-                .html(text.localize('Start trading Forex and CFDs with Binary.com'))
+                .html($('<h1/>', {class: 'notice-msg', html: text.localize('[_1] to your Binary.com account to create a MetaTrader 5 account.', [
+                        '<a class="login_link" href="javascript:;">' + text.localize('Log in') + '</a>'
+                    ])}))
                 .html($('<p/>', {class: 'notice-msg', html: text.localize('[_1] to your Binary.com account to create a MetaTrader 5 account.', [
                         '<a class="login_link" href="javascript:;">' + text.localize('Log in') + '</a>'
                     ])}));
