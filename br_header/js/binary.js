e58f6c1a63bb7f5212fb813fa5124035d059e72b
kellybinary <kelly@binary.com>
2016-12-29 16:52:29 +0800

    Auto-generated commit

--- br_header/js/binary.js
+++ br_header/js/binary.js
@@ -16148,10 +16148,10 @@ Client.prototype = {
     show_login_if_logout: function(shouldReplacePageContents) {
         if (!this.is_logged_in && shouldReplacePageContents) {
             $('#content > .container').addClass('center-text')
-                .html($('<p/>', { class: 'notice-msg', html : text.localize('[_1] to your Binary.com account to create an MT5 account', [
+                .html($('<p/>', { class: 'notice-msg', html : text.localize('[_1] to your Binary.com account to create an MT5 account <br/> Don\'t have a Binary.com account? <a href="[_2]">Create one</a> now', [
                     '<a class="login_link" href="javascript:;">' + text.localize('Log in') + '</a>'])}))
                 .prepend($('<h3/>', { html: text.localize('Your existing Binary.com account and cashier will be linked to your MT5 account')}))
-                .prepend($('<h1/>', { html: text.localize('Start trading Forex and CFDs with Binary.com')}))
+                .prepend($('<h1/>', { html: text.localize('Start trading Forex and CFDs with MetaTrader 5')}))
                 .append($('<p/>', {class:'notice-msg', html: text.localize('Don\'t have a Binary.com account? <a href="[_1]">Create one</a> now', [
                     page.url.url_for('home', '', true)])}));
             $('.login_link').click(function(){Login.redirect_to_login();});
@@ -16705,7 +16705,7 @@ var Page = function(config) {
     this._lang = null;
     onLoad.queue(GTM.push_data_layer);
     $('#logo').on('click', function() {
-        load_with_pjax(page.url.url_for(page.client.is_logged_in ? 'metatrader' : 'home'));
+        load_with_pjax(page.url.url_for(page.client.is_logged_in ? 'metatrader' : ''));
     });
 };
 
