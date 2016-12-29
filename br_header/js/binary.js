7de86999d58bd3e7745830cf033b8c80225e2a49
kellybinary <kelly@binary.com>
2016-12-29 17:10:01 +0800

    Auto-generated commit

--- br_header/js/binary.js
+++ br_header/js/binary.js
@@ -16152,9 +16152,10 @@ Client.prototype = {
                     text.localize('[_1] to your Binary.com account to create an MT5 account <br/> Don\'t have a Binary.com account? <a href="[_2]">Create one</a> now', [
                     '<a class="login_link" href="javascript:;">' + text.localize('Log in') + '</a>'], [page.url.url_for('home', '', true)])}))
                 .prepend($('<h3/>', { html: text.localize('Take advantage of MT5’s advanced features and tools for a complete trading experience.')}))
-                .prepend($('<h1/>', { html: text.localize('Start trading Forex and CFDs with MetaTrader 5')}))
-                .append($('<p/>', {class:'notice-msg', html: text.localize('Don\'t have a Binary.com account? <a href="[_1]">Create one</a> now', [
-                    page.url.url_for('home', '', true)])}));
+                .prepend($('<h1/>', { html: text.localize('Start trading Forex and CFDs with MetaTrader 5')}));
+                // .append($('<p/>', {class:'notice-msg', html: text.localize('Don\'t have a Binary.com account? <a href="[_1]">Create one</a> now', [
+                //     page.url.url_for('home', '', true)])}));
+
             $('.login_link').click(function(){Login.redirect_to_login();});
         }
         return !this.is_logged_in;
