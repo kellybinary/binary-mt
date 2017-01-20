87d6fc2e679385fea6613ad48756b0f8b660e3bd
kelly <kelly@binary.com>
2017-01-20 11:21:23 +0800

    Auto-generated commit

--- br_contract_specs/js/binary.js
+++ br_contract_specs/js/binary.js
@@ -17678,6 +17678,27 @@ pjax_config_page("/terms-and-conditions", function() {
     };
 });
 
+pjax_config_page("/contract-specifications", function() {
+   return {
+       onLoad: function() {
+           var hash;
+           function updateTab() {
+               hash = /^#(forex|volatility)-tab$/.test(window.location.hash) ? window.location.hash : '#cs-tab';
+               $('#cs-menu li').removeClass('active a-active');
+               $('.menu-has-sub-item div.toggle-content').addClass('invisible');
+               $(hash).addClass('active')
+                   .find('a').addClass('a-active');
+               $(hash + '-content').removeClass('invisible');
+           }
+           $(window).on('hashchange', function() {
+               updateTab();
+           });
+           updateTab();
+           $('.content-tab-container').removeClass('invisible');
+       }
+   };
+});
+
 ;pjax_config_page("endpoint", function(){
     return {
         onLoad: function() {
